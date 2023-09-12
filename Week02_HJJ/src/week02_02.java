ea094766b738e6f00f3b23dcc8089e365752e661
hyejun0822 <yus3s@naver.com>
2023-09-12 11:30:44 +0900

    pizza & soccer

---
+++ Week02_HJJ/src/week02_02.java
@@ -0,0 +1,29 @@
+class Sports{
+	String getName() {
+		return "아직 결정되지 않음";
+	}
+	int getPlayers() {
+		return 0;
+	}
+}
+
+class Soccer extends Sports{
+	 
+	 @Override
+	 String getName() {
+		 return "축구";
+	 }
+	 int getPlayers() {
+		 return 11;
+	 }
+}
+public class week02_02 {
+
+	public static void main(String[] args) {
+		Soccer soccer = new Soccer();
+		System.out.println("경기이름 : " + soccer.getName());
+		System.out.println("경기자수 : " + soccer.getPlayers());
+
+	}
+
+}
