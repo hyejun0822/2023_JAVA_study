478c9686e2f95877e4d7059270a01af070a47190
hyejun0822 <yus3s@naver.com>
2023-10-31 09:59:18 +0900

    Chapter 05 commit and push

---
+++ Chapter05/src/Pizza2.java
@@ -0,0 +1,12 @@
+
+public class Pizza2 {
+	private String toppings;
+	private int radius;
+	static final double PI = 3.141592;
+	static int count = 0;
+	
+	public Pizza2(String toppings) {
+		this.toppings = toppings;
+		count++;
+	}
+}
---
+++ Chapter05/src/PizzaTest2.java
@@ -0,0 +1,12 @@
+
+public class PizzaTest2 {
+
+	public static void main(String[] args) {
+		Pizza2 p1 = new Pizza2("Super Supreme");
+		Pizza2 p2 = new Pizza2("Cheese");
+		Pizza2 p3 = new Pizza2("Pepperoni");
+		int n = Pizza2.count;
+		System.out.println("지금까지 판매된 피자 개수 = " + n);
+	}
+
+}
