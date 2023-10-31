1c6fba7e9c6fa69c49d6bcaec5a779d4ef5679fb
hyejun0822 <yus3s@naver.com>
2023-10-31 09:59:53 +0900

    Chapter06 commit and push

---
+++ Chapter06/src/ElectricCarTest.java
@@ -0,0 +1,11 @@
+
+public class ElectricCarTest {
+
+	public static void main(String[] args) {
+		ElectricCar obj = new ElectricCar();
+		obj.speed = 10;
+		obj.setSpeed(60);
+		obj.charge(10);
+	}
+
+}
