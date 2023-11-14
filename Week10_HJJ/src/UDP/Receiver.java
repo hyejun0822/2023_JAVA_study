f217f580627114d9defcbf9c906010540225c430
hyejun0822 <yus3s@naver.com>
2023-11-14 11:43:33 +0900

    Week10 commit&push

---
+++ Week10_HJJ/src/UDP/Receiver.java
@@ -0,0 +1,18 @@
+package UDP;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class Receiver {
+
+	public static void main(String[] args) throws IOException{
+		byte[] buf = new byte[256];
+		
+		DatagramSocket socket = new DatagramSocket(5002);
+		DatagramPacket packet = new DatagramPacket(buf, buf.length);
+		socket.receive(packet);
+		System.out.println(new String(buf));
+	}
+
+}
